3ab37ec7639bb2f11e560ab6371918be6fb2df76
Tanuka <54015607+Tanuka-Mondal@users.noreply.github.com>
2022-08-03 19:43:34 +0530

    Update oop_object.py

--- oop_object.py
+++ oop_object.py
@@ -12,3 +12,12 @@ mob1 = mobile("A12", "Rs. 5000", "Samsung")
 mob1.show()
 mob2 = mobile("iPhone 13", "Rs. 6000", "Apple")
 mob2.show()
+
+"""
+Name:  A12
+Price:  Rs. 5000
+Brand:  Samsung
+Name:  iPhone 13
+Price:  Rs. 6000
+Brand:  Apple
+"""
