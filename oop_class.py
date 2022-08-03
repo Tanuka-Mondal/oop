eadc91b15d1bbd1490d6e934a8de56610ed43c0e
Tanuka <54015607+Tanuka-Mondal@users.noreply.github.com>
2022-08-03 19:36:28 +0530

    Create oop_class.py

---
+++ oop_class.py
@@ -0,0 +1,10 @@
+class mobile:
+    def __init__(self, name, price, brand):
+        self.name = name
+        self.price = price
+        self.brand = brand
+    def show(self):
+        print("Name: ", self.name)
+        print("Price: ", self.price)
+        print("Brand: ", self.brand)
+    
